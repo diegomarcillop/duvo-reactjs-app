ee14614beadb62849598a331e3d4f744d483aabc
Diego Marcillo Pinzon <dfmarcillop@gmail.com>
2020-01-21 15:47:32 -0500

    refactor: carousel component style

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
- 
+
 class Carousel extends Component {
     render() {
         return (
             <div className="">
-                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                    <ol className="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    </ol>
+                <div id="carouselExampleIndicators" className="carousel slide  " data-ride="carousel">
+
                     <div className="carousel-inner">
                         <div className="carousel-item active">
-                            <img src="/img/carrusel1.jpg" className="d-block w-100" alt="..." />
+                            <img src="/img/carrusel1.jpg" className="d-block  img-carrusel" alt="..." />
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Preparate para el  futuro</h5>
+                                <p>Especializate en las carreras del futuro y saca tu mejor potencial.</p>
+                                <button type="button" class="btn btn-outline-success">Cursos</button>
+
+                            </div>
                         </div>
                         <div className="carousel-item">
-                            <img src="/img/carrusel2.jpg"  className="d-block w-100" alt="..." />
+                            <img src="/img/carrusel2_.jpg" className="d-block w-100" alt="..." />
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Trabaja como un profesional</h5>
+                                <p>Adquiere las mejores practicas en la industria del software y aumenta tus posibilidad de empleo</p>
+                                <button type="button" class="btn btn-outline-warning">Comenza ahora</button>
+
+                            </div>
                         </div>
                         <div className="carousel-item">
-                            <img src="/img/carrusel3.jpg" className="d-block w-100" alt="..." />
+                            <img src="/img/carrusel3.jpg" className="d-block w-100 " alt="..." />
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Aumenta el potencial de tu equipo</h5>
+                                <p>Saca el mejor potencial de tu equipo, te ayudamos!</p>
+                                <button type="button" class="btn btn-outline-info">Contacto</button>
+
+                            </div>
                         </div>
                     </div>
                     <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
