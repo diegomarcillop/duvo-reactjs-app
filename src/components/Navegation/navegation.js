116713cd7d904ed1f1775eac570a5ed74e3866c8
Diego Marcillo Pinzon <dfmarcillop@gmail.com>
2020-02-16 23:44:23 -0500

    feat: role functions

--- src/components/Navegation/navegation.js
+++ src/components/Navegation/navegation.js
@@ -1,12 +1,12 @@
-import React, { Component, useContext } from 'react';
+import React, { Component, useContext, Fragment } from 'react';
 import './navegation.css'
 import Item from './items'
 import { UsuarioContext } from '../../Context/usuario-context';
 
-function Navegation(){
+function Navegation() {
 
-    const { user } = useContext(UsuarioContext); 
-    const idRol = user.idRol; 
+    const { user } = useContext(UsuarioContext);
+    const idRol = user.idRol;
     return (
         <div>
             <nav className="navbar sticky-top navbar-expand-lg  nav-dark  navbar-dark  ">
@@ -19,13 +19,27 @@ function Navegation(){
                 </button>
                 <div className="collapse navbar-collapse p-1" id="navbarSupportedContent">
                     <ul className="nav navbar-nav ml-auto">
-                       
+                        {idRol===1 ? ( 
+                             <Fragment>
+                                  <Item name="Inicio" url="/" />
+                                 <Item name="Mis Cursos" url="/" />
+                                 <Item name="Mi Cuenta" url="/" /> 
+                             </Fragment>
+                        ) : (
+                                <Fragment>
+                                    <Item name="Inicio" url="/" />
+                                    <Item name="¿Quienes Somos?" />
+                                    <Item name="Iniciar Sesion" url="/signin" icon="fas fa-user-circle icon-config color-yellow" />
+                                    <Item name="Registrarse" url="/signup" icon="far fa-address-card icon-config" />
+                                </Fragment>
+                            )
+                        }
                     </ul>
-                </div> 
+                </div>
             </nav>
         </div >
     )
 }
- 
+
 
 export default Navegation;
